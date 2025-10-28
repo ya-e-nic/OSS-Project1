e0a8bf33d2f3b37047c97f469d94d130c90a8864
Kim jungHun <kjh20060829@gmail.com>
2025-10-29 07:48:50 +0900

    Update main.c
    숫자 비교 코드 추가

--- main.c
+++ main.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 
+imt num, num2;
+
 int main()
 {
-printf("sw"):
-printf("aaa");
-return 0;
+  printf("Enter a first number"):
+  scanf_s("%d", &num);
+  printf("Enter a second number"):
+  scanf_s("%d", &num2);
+  if (num > num2) printf("first number bigger than second number");
+  else if (num < num2) printf("second number bigger than first number");
+  else printf("first number and second number are same")
+  return 0;
 }
