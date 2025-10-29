834d5c2a654891ef21f61431cb9e46b91f9c800a
Kim jungHun <kjh20060829@gmail.com>
2025-10-29 10:41:55 +0900

    Update main.c
    Added a loop to continuously compare two numbers until both are zero.

--- main.c
+++ main.c
@@ -4,12 +4,16 @@ imt num, num2;
 
 int main()
 {
-  printf("Enter a first number"):
-  scanf_s("%d", &num);
-  printf("Enter a second number"):
-  scanf_s("%d", &num2);
-  if (num > num2) printf("first number bigger than second number");
-  else if (num < num2) printf("second number bigger than first number");
-  else printf("first number and second number are same")
-  return 0;
+  while (1)
+  {
+    printf ("if first number and second number are 0, end");
+    printf("Enter a first number"):
+    scanf_s("%d", &num);
+    printf("Enter a second number"):
+    scanf_s("%d", &num2);
+    if (num == 0 && num2 == 0) return 0;
+    if (num > num2) printf("first number bigger than second number");
+    else if (num < num2) printf("second number bigger than first number");
+    else printf("first number and second number are same");
+  }
 }
