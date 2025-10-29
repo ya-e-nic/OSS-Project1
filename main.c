f36c973f3e2a69259f2ecdae770018b84da5ed29
Kim jungHun <kjh20060829@gmail.com>
2025-10-29 10:45:06 +0900

    Update main.c
    Fix input handling and update exit conditions.

--- main.c
+++ main.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 
 imt num, num2;
+char ch;
 
 int main()
 {
   while (1)
   {
-    printf ("if first number and second number are 0, end");
+    printf ("if first number is e, end");
     printf("Enter a first number"):
-    scanf_s("%d", &num);
+    scanf_s("%c", &ch);
+    num = ch -'0';
     printf("Enter a second number"):
     scanf_s("%d", &num2);
-    if (num == 0 && num2 == 0) return 0;
+    if (ch == 'e') return 0;
     if (num > num2) printf("first number bigger than second number");
     else if (num < num2) printf("second number bigger than first number");
     else printf("first number and second number are same");
